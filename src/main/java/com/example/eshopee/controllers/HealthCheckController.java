6554de116431dfdffac694f9d1298e1b78d8dee7
Jay <jayandratodawat@gmail.com>
2024-09-29 11:35:07 +0530

    Refactor: Change method type  of healthCheck

--- src/main/java/com/example/eshopee/controllers/HealthCheckController.java
+++ src/main/java/com/example/eshopee/controllers/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.example.eshopee.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HealthCheckController {
 
-    @RequestMapping("/health")
+    @GetMapping("/health")
     public String healthCheck() {
         return "Service is up and running";
     }
